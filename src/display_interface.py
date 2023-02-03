de479d1803d3bea857dc76dcb10ce19955645645
Raphael Michel <raphael.michel@cetic.be>
2023-02-03 22:43:31 +0100

    Simple static counter interface Refactoring of display_id validation

--- src/display_interface.py
+++ src/display_interface.py
@@ -1,27 +1,88 @@
 # Interface to manage various kinds of hardware display
+import enum
+
 import displays.manager
 
+class CounterType(enum.Enum):
+    NUMERIC = 0,
+    PROGRESS_BAR = 1
 
 class DisplayInterface:
     def __init__(self, hardware_display):
         # Keep Hardware Display
         self.__display = hardware_display
         self.__nb_displays = hardware_display.get_available_displays()
+        self.__counters = {}
 
+    def __validate_display_id(self, display_id):
+        if display_id > self.__display.get_available_displays() - 1 or display_id < 0:
+            raise ValueError("Invalid display id")
     def get_available_displays(self):
         return str(self.__nb_displays)
 
     def set_cursor_position(self, display_id, position):
-        if display_id > self.__display.get_available_displays() - 1 or display_id < 0:
-            raise ValueError("Invalid display id")
+        self.__validate_display_id(display_id)
         self.__display.set_cursor_position(display_id, int(position['x']), int(position['y']))
 
     def print_message(self, display_id, message):
-        if display_id > self.__display.get_available_displays() - 1 or display_id < 0:
-            raise ValueError("Invalid display id")
+        self.__validate_display_id(display_id)
         self.__display.print_message(display_id, message)
 
     def clear(self, display_id):
-        if display_id > self.__display.get_available_displays() - 1 or display_id < 0:
-            raise ValueError("Invalid display id")
+        self.__validate_display_id(display_id)
         self.__display.clear(display_id)
+
+    def init_counter(self, display_id, label, label_position=(0,0), value_position=(0,1), counter_type:CounterType=CounterType.NUMERIC, initial_value=0, default_step=1, min_value=None, max_value=None):
+        self.__validate_display_id(display_id)
+
+        if min_value is not None and initial_value < min_value:
+            raise ValueError(f'Initial counter value {initial_value} below minimum {min_value}')
+
+        if max_value is not None and initial_value > max_value:
+            raise ValueError(f'Initial counter value {initial_value} above maximum {max_value}')
+
+        self.__counters[display_id] = dict(initial_value=initial_value,
+                                           current_value=initial_value,
+                                           default_step=default_step,
+                                           min_value=min_value,
+                                           max_value=max_value,
+                                           label=label,
+                                           label_position=label_position,
+                                           value_position=value_position,
+                                           counter_type=counter_type
+                                        )
+
+        #TODO: Display counter
+
+    def step_counter(self, display_id, step=None):
+        self.__validate_display_id()
+        if display_id in self.__counters:
+            current_value = self.__counters.get(display_id).get('current_value')
+            step_value = step if step is not None else self.__counters.get(display_id).get('default_step')
+            self.set_counter_value(display_id, (current_value+step_value))
+        else:
+            raise ValueError('No counter found for this display')
+
+
+    def set_counter_value(self, display_id, value):
+        self.__validate_display_id()
+        if display_id in self.__counters:
+            counter = self.__counters.get(display_id)
+            if (value is None) or (counter['min_value'] is not None and value <= counter['min_value']) or (counter['max_value'] is not None and value >= counter['min_value']):
+                raise ValueError(f"Counter value {value} is out of bounds [{counter['min_value']}, {counter['max_value']}]")
+
+            self.__counters[display_id]['current_value'] = value
+            #TODO: Update display
+        else:
+            raise ValueError('No counter found for this display')
+    def reset_counter(self, display_id):
+        self.__validate_display_id()
+        if display_id in self.__counters:
+            counter = self.__counters.get(display_id)
+            self.set_counter_value(display_id, counter['initial_value'])
+
+    def remove_counter(self, display_id):
+        self.__validate_display_id()
+        if display_id in self.__counters:
+            del self.__counters[display_id]
+            #TODO: Update display
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request
 import display_interface
 import displays.ba6x_manager
@@ -20,19 +22,16 @@ def set_cursor(display_id):
     except ValueError as e:
         return str(e), 400
 
-
 @display_api.route('/display/<int:display_id>/write', methods=['POST'])
 def write_message(display_id):
     try:
         # request data needs to be converted to avoid b'" display
         # TODO: auto decode
         message = request.data.decode('UTF-8')
-
         di.print_message(display_id, message)
         return 'wilco', 202
     except ValueError as e:
         return str(e), 400
-
 
 @display_api.route('/display/<int:display_id>/clear', methods=['POST'])
 def clear_display(display_id):
@@ -41,6 +40,47 @@ def clear_display(display_id):
         return 'wilco', 202
     except ValueError as e:
         return str(e), 400
+
+
+@display_api.route('/display/<int:display_id>/counter', methods=['POST'])
+def init_counter(display_id):
+    try:
+        data = json.parse(request.data.decode('UTF-8'))
+        #TODO : clean data dict before calling
+        di.init_counter(display_id, **data)
+    except ValueError as e:
+        return str(e), 400
+    except Exception as e:
+        return str(e), 500
+
+@display_api.route('/display/<int:display_id>/counter/step', methods=['POST'])
+def step_counter(display_id):
+    try:
+        args = request.args.to_dict() if request.args else dict()
+        di.step_counter(display_id, args.get('v', None))
+    except ValueError as e:
+        return str(e), 400
+    except Exception as e:
+        return str(e), 500
+
+@display_api.route('/display/<int:display_id>/counter', methods=['PUT'])
+def set_counter_value(display_id):
+    try:
+        args = request.args.to_dict() if request.args else dict()
+        di.set_counter_value(display_id, args.get('v', None))
+    except ValueError as e:
+        return str(e), 400
+    except Exception as e:
+        return str(e), 500
+
+@display_api.route('/display/<int:display_id>/counter', methods=['DELETE'])
+def step_counter(display_id):
+    try:
+        di.remove_counter(display_id)
+    except ValueError as e:
+        return str(e), 400
+    except Exception as e:
+        return str(e), 500
 
 
 @display_api.route('/brew', methods=['POST', 'BREW'])
